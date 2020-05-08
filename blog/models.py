99b18121bd43de71b1a4daf4210e6544fa44644c
sonagi784 <sonagi784@gmail.com>
2020-05-08 23:27:09 +0900

    20120508 study

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.title) + ' :: ' + str(self.author)
