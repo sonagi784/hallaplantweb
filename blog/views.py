ea30927bb6261d3b21815ec813fb6a98fce575cc
sonagi784 <sonagi784@gmail.com>
2020-05-12 11:46:28 +0900

    change index to PostList

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
+class PostList(ListView):
+    model = Post
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+
+"""
 def index(request):
     posts = Post.objects.all()
     return render(
@@ -11,3 +18,4 @@ def index(request):
             'a_plus_b' : 1 + 3,
         },
     )
+"""
