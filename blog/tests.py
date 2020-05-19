698c21849cd387ce6d4d8d1c4e2c45e684291b4b
sonagi784 <sonagi784@gmail.com>
2020-05-19 17:27:40 +0900

    test A

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 
-# Create your tests here.
+class TestView(TestCase):
+    def setup(self):
+        self.client = Client()
+    
+    def test_post_list(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.title
+        
+        self.assertEqual(title.text, 'blog')
+
+        navbar = soup.find('div', id='navbar')
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        self.assertEqual(Post.objects.count(), 0)
+        self.assertIn('아직 게시물이 없습니다', soup.body.text)
+
+        post_000 = Post.objects.create(
+            title = 'test post',
+            content= 'hello world',
+
+        )
+
+        self.assertGreater(Post.objects.count(), 0)
+        
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
     model = Post
     def get_queryset(self):
         return Post.objects.order_by('-created')
 
+class PostDetail(DetailView):
+    model = Post
+
 """
+def post_detail(request, pk):
+    blog_post = Post.objects.get(pk=pk)
+    return render(
+        request,
+        'blog/post_detail.html',
+        {
+            'blog_post': blog_post,
+        }
+    )
+
 def index(request):
     posts = Post.objects.all()
     return render(
         request,
-        'blog/index.html',
+        'blog/test.html',
         {
             'posts' : posts,
         },
     )
 """
+
