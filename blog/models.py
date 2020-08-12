08d855db805e106f9f27cac4f00762d386ffaae0
sonagi784 <sonagi784@gmail.com>
2020-08-12 21:15:49 +0900

    test.py

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -11,3 +10,6 @@ class Post(models.Model):
 
     def __str__(self):
         return str(self.title) + ' :: ' + str(self.author)
+
+    def get_absolute_url(self):
+        return '/blog/{}/'.format(self.pk)
--- blog/tests.py
+++ blog/tests.py
@@ -4,31 +4,66 @@ from .models import Post
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+def create_post(title, content, author):
+    blog_post = Post.objects.create(
+            title = title,
+            content = content,
+            created = timezone.now(),
+            author = author,
+        )
+    return blog_post
+
+
 class TestView(TestCase):
-    def setup(self):
+    def setUp(self):
         self.client = Client()
-        self.author_000 = User.objects.create(username='Smith', password='nopassword')
+        self.author_000 = User.objects.create(username='smith', password='nopassword')
+
+    def check_navbar(self, soup):
+        navbar = soup.find('div', id='navbar')
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)    
 
     def test_post_list(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
-        title = soup.title
-        
-        self.assertEqual(title.text, 'blog')
 
-        navbar = soup.find('div', id='navbar')
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.assertEqual(soup.title.text, 'blog')
 
-        self.assertEqual(Post.objects.count(), 0)
+        self.check_navbar(soup)
+
+        self.assertEqual(Post.objects.count(), 0) # 실제 db는 고려하지 않음
         self.assertIn('아직 게시물이 없습니다', soup.body.text)
 
-        post_000 = Post.objects.create(
-            title = 'test post',
-            content = 'hello world',
-            created = timezone.now(),
-            author = self.author_000,
+        #post.objects.all, get, count, create, order_by ..
+        post_000 = create_post(
+            'first post', 
+            'we are the world', 
+            self.author_000,
         )
-
         self.assertGreater(Post.objects.count(), 0)
+        
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        #body = soup.body
+        self.assertNotIn('아직 게시물이 없습니다', soup.body.text)
+        self.assertIn(post_000.title, soup.body.text)
+    
+    def test_post_detail(self): # 새 함수 시작마다 새로운 db
+        post_000 = create_post(
+            'first post', 
+            'we are the world', 
+            self.author_000,
+        )
+        self.assertGreater(Post.objects.count(), 0)
+        self.assertEqual(post_000.get_absolute_url(), '/blog/{}/'.format(post_000.pk))
+        
+        response = self.client.get(post_000.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        self.assertEqual(soup.title.text, '{} - blog'.format(post_000.title))
+
+        self.check_navbar(soup)
