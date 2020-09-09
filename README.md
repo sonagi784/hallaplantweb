c9bb20cf209446dd59faf6d862aad66463864c49
sonagi784 <sonagi784@gmail.com>
2020-09-10 01:30:55 +0900

    blog preview

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 https://startbootstrap.com/templates/modern-business/
 startbootstrap의 'modern business' template을 적절히 수정하여 만들었습니다.
 
-1. container 하나로 묶여있던 main page부분을 section별로 나눠 배경이 white와 grey가 번갈아 나오게끔 수정하였습니다.
+1. git을 public으로 바꿀때 settings.py 의 secretkey를 삭제할것
+2. container 하나로 묶여있던 main page부분을 section별로 나눠 배경이 white와 grey가 번갈아 나오게끔 수정했음
--- main/views.py
+++ main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def index(request):
+    if len(Post.objects.all()) >= 3:
+        posts = Post.objects.order_by('-created')[0:3]
+    else:
+        posts = Post.objects.order_by('-created')
+
     return render(
         request, 
         'main/index.html',
+        {
+            'posts': posts,
+        }
     )
