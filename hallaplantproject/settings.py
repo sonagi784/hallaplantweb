d4175aae641fb15057021171ef462471adb5de18
sonagi784 <sonagi784@gmail.com>
2020-09-15 06:00:07 +0900

    secret key, blog nav

--- hallaplantproject/settings.py
+++ hallaplantproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = # removed
+SECRET_KEY = '-k$^s)ay9gk^5*r%a%2bob9_-wmdri*1cxbd3u$&ju@n=z_s_d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
