43ae77d7af6254b9c83a88ba5518820137a922d3
sonagi784 <sonagi784@gmail.com>
2021-06-03 01:14:36 +0900

    main/views FunctionBasedView to ClassBasedView

--- main/views.py
+++ main/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from blog.models import Post
+
+# def index(request):
+#     if len(Post.objects.all()) >= 3:
+#         posts = Post.objects.order_by('-created')[0:3]
+#     else:
+#         posts = Post.objects.order_by('-created')
+
+#     return render(
+#         request, 
+#         'main/index.html',
+#         {
+#             'post_preview': posts,
+#         }
+#     )
+
 from blog.models import Post
+from django.views.generic.base import TemplateView
 
-def index(request):
-    if len(Post.objects.all()) >= 3:
-        posts = Post.objects.order_by('-created')[0:3]
-    else:
-        posts = Post.objects.order_by('-created')
+class PreviewList(TemplateView):
+    template_name = "main/index.html"
 
-    return render(
-        request, 
-        'main/index.html',
-        {
-            'posts': posts,
-        }
-    )
+    def get_context_data(self, *, object_list=None, **kwargs): # context에 다른 객체들을 넣어 보낼수있다
+        context = super(PreviewList, self).get_context_data(**kwargs)
+        context['post_preview'] = Post.objects.order_by('-created')[:3] # 카테고리 객체들
+
+        return context
